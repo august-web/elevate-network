bf641b9c1e6bf0e9f9fee1f17696904c01163923
Augustine Chima <elon.dev18@gmail.com>
2026-09-12 23:15:57 +0000

    refactor: remove sanity schema index

--- sanity/schemas/index.ts
+++
@@ -1,6 +0,0 @@
-import post from "./post";
-import event from "./event";
-import program from "./program";
-import teamMember from "./teamMember";
-
-export const schemaTypes = [post, event, program, teamMember];
